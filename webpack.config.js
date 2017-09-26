b367465e9944a844774341b68ad71e916c9f6b82
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-26 15:13:27 +0800

    modfiy webpack config
    1. keep only necessary loaders
    2. 2-space indentation
    3. wipe out comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,37 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: __dirname + '/src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + 'build'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader" }
-                ]
-            }
+  devtool: 'eval-source-map',
+  entry: __dirname + '/app/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist') + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
         ]
-    },
-    devServer: {
-        contentBase: './build',
-        // colors: true,
-        historyApiFallback: true,
-        inline: true,
-        port: 3000
-        // process: true
-    }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './build',
+    historyApiFallback: true,
+    inline: true,
+    port: 3000
+  }
 }
