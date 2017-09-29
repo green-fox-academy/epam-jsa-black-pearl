a716ed1d078bbc4016fffcea3fa44a6ab1200b62
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-29 11:29:52 +0800

    modify intendation and lf

--- src/server/heartbeat.js
+++ src/server/heartbeat.js
@@ -7,26 +7,26 @@ var db = require('../../.environment.json');
 var url = 'mongodb://' + db.database.url + ':' + db.database.port + '/' + db.database.database;
 
 function heartbeat(callback) {
-    MongoClient.connect(url, function (err, database) {
-        if (err) {
-            callback('error');
-            return;
-        }
-        console.log('Connection established to ' + url);
-        let collection = database.collection(db.database.collection);
-        collection.find({}).toArray(function (err, result) {
-            if (err) {
-                callback('error');
-                return;
-            }
-            if (result.length === 0) {
-                callback('error');
-            } else {
-                callback('ok');
-            }
-            database.close();
-        });
+  MongoClient.connect(url, function (err, database) {
+    if (err) {
+      callback('error');
+      return;
+    }
+    console.log('Connection established to ' + url);
+    let collection = database.collection(db.database.collection);
+    collection.find({}).toArray(function (err, result) {
+      if (err) {
+        callback('error');
+        return;
+      }
+      if (result.length === 0) {
+        callback('error');
+      } else {
+        callback('ok');
+      }
+      database.close();
     });
+  });
 }
 
 module.exports = heartbeat;
