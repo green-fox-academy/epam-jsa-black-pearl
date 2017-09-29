a236a7fea2b97aae7ce08526e5ae5566f2640e4d
ChrisH404 <huangchengcq@gmail.com>
2017-09-29 16:38:13 +0800

    BLP-10 & 11

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/client/index.js'),
@@ -34,4 +37,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin([path.resolve(__dirname, './dist')]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/client/index.html'),
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new ExtractTextPlugin('style.css'),
+  ],
 };
