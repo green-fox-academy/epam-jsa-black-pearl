f27c4406e233ab9facb00143d3eeeb997ec25c35
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-29 11:47:09 +0800

    var, let to const

--- src/server/heartbeat.js
+++ src/server/heartbeat.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
+const MongoClient = mongodb.MongoClient;
 
 var db = require('../../.environment.json');
 var url = 'mongodb://' + db.database.url + ':' + db.database.port + '/' + db.database.database;
@@ -13,7 +13,7 @@ function heartbeat(callback) {
       return;
     }
     console.log('Connection established to ' + url);
-    let collection = database.collection(db.database.collection);
+    const collection = database.collection(db.database.collection);
     collection.find({}).toArray(function (err, result) {
       if (err) {
         callback('error');
