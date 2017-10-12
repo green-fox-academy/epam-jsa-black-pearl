50ce8154efbb22736f3dca866ea728f338428a5f
ChrisH404 <huangchengcq@gmail.com>
2017-10-12 13:29:29 +0800

    heroku bug fix

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Login from '../componets/Login';
+import Login from '../components/Login';
 
 class App extends React.Component {
   render() {
