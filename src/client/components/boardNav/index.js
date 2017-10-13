a8202222e9d590b52bd708a394678c8a0d3b642b
Alfred Wei <35080930@qq.com>
2017-10-13 11:21:55 +0800

    css 0.1

--- src/client/components/boardNav/index.js
+++ src/client/components/boardNav/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './index.scss';
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
-  };
+  }
   render() {
     return (
-      <nav>
-        <button name="Navi"></button>
-        <input id="searchBar" required type="text" name="searchBar" />
-        <p>Black Pearl</p>
-        <Link to={'./login'}>Logout</Link>
-      </nav>
+      <div>
+        <nav>
+          <button></button>
+          <p>BOARD</p>
+          <input id="searchBar" required type="text" name="searchBar" />
+          <p>Black Pearl</p>
+          <Link to={'./login'}>Logout</Link>
+        </nav>
+      </div>
     )
   }
 }
