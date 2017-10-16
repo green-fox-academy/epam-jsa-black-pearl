63873040b55ba6a947d766d6cd1d56a0f0b47150
AlexanderZhao <zhaomengzhe@qq.com>
2017-10-16 14:20:18 +0800

    fix eslinter problems

--- src/client/components/loginForm/index.js
+++ src/client/components/loginForm/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router';
+import {Redirect} from 'react-router';
 import Validator from 'validator';
 
 import './index.scss';
 import Spinner from '../../../img/spinner.svg';
+
+const MIN_PASSWORD_LENGTH = 6;
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -16,106 +18,105 @@ class LoginForm extends React.Component {
       isLoggedIn: false,
     };
   }
+
   onLogin() {
     function isValidEmail(email) {
       return Validator.isEmail(email);
     }
+
     function isValidPassword(password) {
-      return password.length >= 6;
+      return password.length >= MIN_PASSWORD_LENGTH;
     }
-    if (!isValidEmail(this.state.username) || !isValidPassword(this.state.password)) {
-      let that = this;
-      that.setState({ isInvalidFields: true });
-      setTimeout(function () {
-        that.setState({ isInvalidFields: false });
-      }, 500);
-      return;
-    }
-    this.setState({
-      isLoading: true,
-    });
-    let that = this;
-    doLogin(that, this.state.username, this.state.password);
+
     function doLogin(state, username, password) {
       let API = '/api/login';
+
       let myHeaders = new Headers();
+
       myHeaders.append('Content-Type', 'application/json');
       let myRequest = new Request(API, {
         'method': 'POST',
         'headers': myHeaders,
-        'body': JSON.stringify({ 'username': username, 'password': password }),
+        'body': JSON.stringify({'username': username, 'password': password}),
       });
-      fetch(myRequest).then(function (response) {
+
+      fetch(myRequest).then((response) => {
         if (response.status === 200) return response.json();
         throw new Error('error.');
-      }).then(function (response) {
+      }).then((response) => {
         localStorage.token = response.token;
-        state.setState({
-          isLoading: false,
-          isLoggedIn: true,
-        });
-      }).catch(function (error) {
-        state.setState({
-          isLoading: false,
-        });
+        state.setState({isLoading: false, isLoggedIn: true});
+      }).catch((error) => {
+        state.setState({isLoading: false});
       });
     }
+    if (!isValidEmail(this.state.username) ||
+    !isValidPassword(this.state.password)) {
+      let that = this;
+
+      that.setState({isInvalidFields: true});
+      setTimeout(() => {
+        that.setState({isInvalidFields: false});
+      }, 500);
+      return;
+    }
+    this.setState({isLoading: true});
+    let that = this;
+
+    doLogin(that, this.state.username, this.state.password);
   }
   onUsernameChange(ev) {
-    this.setState({
-      username: ev.target.value,
-    });
+    this.setState({username: ev.target.value});
   }
   onPasswordChange(ev) {
-    this.setState({
-      password: ev.target.value,
-    });
+    this.setState({password: ev.target.value});
   }
   render() {
     if (this.state.isLoggedIn) {
       return (
-        <Redirect to='/board' />
-      )
-    } else {
-      let button = null;
-      if (!this.state.isLoading) {
-        button = (
-          <div>
-            <input type="button" value="Login"
-              onClick={this.onLogin.bind(this)}
-              className={this.state.isInvalidFields ? 'shaking' : ''} />
-          </div>
-        );
-      } else {
-        button = (
-          <div>
-            <div className="loadingInput">
-              <Spinner width={50} height={50} />
-            </div>
-          </div>
-        );
-      }
-      let warning = null;
-      if (this.state.isLoginFailure) {
-        warning = (
-          <p>Login failed.</p>
-        );
-      } else {
-        warning = null;
-      }
-      return (
-        <form className="login-form">
-          <div className="warning">
-            {warning}
-          </div>
-          <input type="text" placeholder="Email"
-            onChange={this.onUsernameChange.bind(this)} />
-          <input type="password" placeholder="Password"
-            onChange={this.onPasswordChange.bind(this)} />
-          {button}
-        </form>
+        <Redirect to="/board" />
       );
     }
+    let button = null;
+
+    if (!this.state.isLoading) {
+      button = (
+        <div>
+          <input type="button" value="Login"
+            onClick={this.onLogin.bind(this)}
+            className={this.state.isInvalidFields ? 'shaking' : ''} />
+        </div>
+      );
+    } else {
+      button = (
+        <div>
+          <div className="loadingInput">
+            <Spinner width={50} height={50} />
+          </div>
+        </div>
+      );
+    }
+    let warning = null;
+
+    if (this.state.isLoginFailure) {
+      warning = (
+        <p>Login failed.</p>
+      );
+    } else {
+      warning = null;
+    }
+    return (
+      <form className="login-form">
+        <div className="warning">
+          {warning}
+        </div>
+        <input type="text" placeholder="Email"
+          onChange={this.onUsernameChange.bind(this)} />
+        <input type="password" placeholder="Password"
+          onChange={this.onPasswordChange.bind(this)} />
+        {button}
+      </form>
+    );
   }
 }
 
