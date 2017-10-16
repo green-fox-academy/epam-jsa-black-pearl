dfc8c01461dbf01162de9bc6325aaa796767d0a3
ChrisH404 <huangchengcq@gmail.com>
2017-10-16 16:19:05 +0800

    isLoggedIn function refector

--- src/client/controller/isLoggedIn.js
+++ src/client/controller/isLoggedIn.js
@@ -3,18 +3,14 @@ import Jwt from 'jwt-simple';
 const jwtTokenSecret = 'black_pearl';
 
 function isLoggedIn() {
-  if (localStorage.token) {
-    try {
-      let decoded = Jwt.decode(localStorage.token, jwtTokenSecret);
+  if (!localStorage.token) {
+    return false;
+  }
+  try {
+    let decoded = Jwt.decode(localStorage.token, jwtTokenSecret);
 
-      if (decoded.exp <= Date.now()) {
-        return false;
-      }
-      return true;
-    } catch (err) {
-      return false;
-    }
-  } else {
+    return decoded.exp > Date.now();
+  } catch (err) {
     return false;
   }
 }
