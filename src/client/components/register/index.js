2ea5857f8d23c63b1ea67fabee18e10fd44255f0
AlexanderZhao <zhaomengzhe@qq.com>
2017-10-17 17:16:13 +0800

    redirect /register to /board if loginned

--- src/client/components/register/index.js
+++ src/client/components/register/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
+
 import './index.scss';
 import RegisterForm from '../registerForm';
 import img from '../../../img/black_pearl.png';
+import isLoggedIn from '../../controller/isLoggedIn';
 
 class Register extends React.Component {
   render() {
+    if (isLoggedIn()) {
+      return (
+        <Redirect to="/board" />
+      );
+    }
     return (
       <main>
         <header>
