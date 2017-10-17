c9ea418ad33e6dc9adccb4ab87f9414debdadade
AlfredWei0420 <31198072+AlfredWei0420@users.noreply.github.com>
2017-10-18 00:18:28 +0800

    enable recaptcha

--- src/client/components/register/index.js
+++ src/client/components/register/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import {Link} from 'react-router-dom';
 import './index.scss';
 import RegisterForm from '../registerForm';
 import img from '../../../img/black_pearl.png';
+
+function onChange(value) {
+  console.log('Captcha value:', value);
+}
 
 class Register extends React.Component {
   render() {
@@ -13,6 +18,9 @@ class Register extends React.Component {
           <h1>Sign Up</h1>
         </header>
         <RegisterForm />
+        <div className="recaptcha">
+          <ReCAPTCHA ref="recaptcha" sitekey="6LfMtzQUAAAAAMD920qYn8GBmjBKgv5QeOW_u2gH" onChange={onChange} />,
+        </div>
         <p>Already have an account? <Link to={'/login'}>Login here</Link></p>
       </main>
     );
