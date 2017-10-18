d2e6e3ae132277b0544260b6b1c82bd19b46a7d3
AlexanderZhao <zhaomengzhe@qq.com>
2017-10-18 16:39:10 +0800

    add column component

--- src/client/components/boardColumn/index.js
+++ src/client/components/boardColumn/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './index.scss';
-// import Card from '../boardCard';
+import Card from '../boardCard';
 
 class List extends React.Component {
   render() {
@@ -12,6 +12,10 @@ class List extends React.Component {
             <div className="edit-icon"></div>
             <div className="column-title">To-do</div>
             <div className="column-card-count">2</div>
+          </div>
+          <div>
+            <Card />
+            <Card />
           </div>
         </div>
       </div>
