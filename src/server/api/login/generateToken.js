017369fff942da26bc67352cdd52136efb53d91b
ChrisHuang <huangzhengcq@gmail.com>
2017-10-19 02:50:30 -0500

    feat(app): verify jwt function (#40)
    * put JWT Key in .env file
    
    * remove jwt key in frontend token checking
    
    * verify token in request headers

--- src/server/api/login/generateToken.js
+++ src/server/api/login/generateToken.js
@@ -1,22 +1,18 @@
 'use strict';
 
-const express = require('express');
 const jwt = require('jwt-simple');
 const moment = require('moment');
 const envConst = require('../../envConst');
 
-const app = express();
 const expireDate = 7;
-
-app.set('jwtTokenSecret', envConst.getJwtKey());
+const jwtTokenSecret = envConst.getJwtKey();
 
 function generateToken(username) {
   let expires = moment().add(expireDate, 'days').valueOf();
   let token = jwt.encode({
     iss: username,
     exp: expires,
-  },
-  app.get('jwtTokenSecret'));
+  }, jwtTokenSecret);
 
   return {
     token: token,
