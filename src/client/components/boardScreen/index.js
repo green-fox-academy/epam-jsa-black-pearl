61b2f4c58508cf769ebb3c69973ef2f0736583ab
AlexanderZhao <zhaomengzhe@qq.com>
2017-10-24 21:10:15 +0800

    change name and add backend request

--- src/client/components/boardScreen/index.js
+++ src/client/components/boardScreen/index.js
@@ -133,8 +133,9 @@ class BoardScreen extends React.Component {
 
   generateWelcomeList() {
     return (
-      <div className="board-welcome-list">
-        <p className="board-name">Welcome onboard...</p>
+      <div className="board-welcome-list"
+        onClick={this.showBoardDetail.bind(this, 'welcome')}>
+        <p className="board-name">WELCOME BOARD</p>
       </div>
     );
   }
