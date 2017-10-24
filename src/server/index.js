086ce8e8c8ce6241a084c351e145e9a0a0cbdc8f
ChrisH404 <huangchengcq@gmail.com>
2017-10-24 15:38:11 +0800

    create columns api finish

--- src/server/index.js
+++ src/server/index.js
@@ -23,11 +23,12 @@ const STATUS_FORBIDDEN = 403;
 const CONFLICT = 409;
 const forbiddenMessage = 'User does not exists or bad credential!';
 const SERVER_ERROR_MESSAGE = 'Something went wrong!';
-const LOGIN_MESSAGE = 'Please login first!';
+const NOT_LOGIN_MESSAGE = 'Please login first!';
+const CONTENT_TYPE_ERROR_MESSAGE = 'Content type error!';
+const MISSING_FIELD_MESSAGE = 'Missing field(s)!';
 const NO_BOARD_MESSAGE = 'No such board found!';
 const DELETE_BOARD_MESSAGE = 'Delete board success!';
 const NO_INFO = 0;
-
 
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 app.use(bodyParser.json());
@@ -38,9 +39,9 @@ router.get('/', function(req, res) {
 
 router.post('/login', function(req, res) {
   if (!req.is('application/json')) {
-    res.status(BAD_REQUEST).json({message: 'Content type error!'});
+    res.status(BAD_REQUEST).json({message: CONTENT_TYPE_ERROR_MESSAGE});
   } else if (!req.body.username || !req.body.password) {
-    res.status(BAD_REQUEST).json({message: 'Missing field(s)!'});
+    res.status(BAD_REQUEST).json({message: MISSING_FIELD_MESSAGE});
   } else {
     auth(req.body, function(result) {
       if (result === 'error' || !result) {
@@ -56,9 +57,9 @@ router.post('/login', function(req, res) {
 
 router.post('/register', function(req, res) {
   if (!req.is('application/json')) {
-    res.status(BAD_REQUEST).json({message: 'Content type error!'});
+    res.status(BAD_REQUEST).json({message: CONTENT_TYPE_ERROR_MESSAGE});
   } else if (!req.body.username || !req.body.password) {
-    res.status(BAD_REQUEST).json({message: 'Missing field(s)!'});
+    res.status(BAD_REQUEST).json({message: MISSING_FIELD_MESSAGE});
   } else {
     register(req.body, function(result) {
       if (result === 'error' || !result) {
@@ -76,7 +77,7 @@ router.get('/boards', function(req, res) {
   let username = jwtVerify(req.headers.token);
 
   if (!username) {
-    res.status(STATUS_FORBIDDEN).json({message: 'Please login first!'});
+    res.status(STATUS_FORBIDDEN).json({message: NOT_LOGIN_MESSAGE});
   } else {
     boards.getBoardsByUser(username, function(result) {
       if (result === 'error' || !result) {
@@ -93,11 +94,14 @@ router.get('/boards/:id', function(req, res) {
   let boardId = req.params.id;
 
   if (!username) {
-    res.status(STATUS_FORBIDDEN).json({message: 'Please login first!'});
+    res.status(STATUS_FORBIDDEN).json({message: NOT_LOGIN_MESSAGE});
   } else {
     boards.getBoardById(username, boardId, function(result) {
-      if (result === 'error' || !result) {
+      console.log(result);
+      if (result === 'error') {
         res.status(INTERNAL_SERVER_ERROR).json({message: SERVER_ERROR_MESSAGE});
+      } else if (result === 'notFound' || !result) {
+        res.json({message: NO_BOARD_MESSAGE});
       } else {
         res.json(result);
       }
@@ -109,11 +113,11 @@ router.post('/boards', function(req, res) {
   let username = jwtVerify(req.headers.token);
 
   if (!req.is('application/json')) {
-    res.status(BAD_REQUEST).json({message: 'Content type error!'});
+    res.status(BAD_REQUEST).json({message: CONTENT_TYPE_ERROR_MESSAGE});
   } else if (!req.body.boardname) {
-    res.status(BAD_REQUEST).json({message: 'Missing field(s)!'});
+    res.status(BAD_REQUEST).json({message: MISSING_FIELD_MESSAGE});
   } else if (!username) {
-    res.status(STATUS_FORBIDDEN).json({message: 'Please login first!'});
+    res.status(STATUS_FORBIDDEN).json({message: NOT_LOGIN_MESSAGE});
   } else {
     boards.createNewBoard(req.body, username, function(result) {
       if (result === 'error' || !result) {
@@ -127,6 +131,25 @@ router.post('/boards', function(req, res) {
 
 router.post('/boards/:id/columns', function(req, res) {
   let username = jwtVerify(req.headers.token);
+  let boardId = req.params.id;
+
+  if (!req.is('application/json')) {
+    res.status(BAD_REQUEST).json({message: CONTENT_TYPE_ERROR_MESSAGE});
+  } else if (!req.body.columnName) {
+    res.status(BAD_REQUEST).json({message: MISSING_FIELD_MESSAGE});
+  } else if (!username) {
+    res.status(STATUS_FORBIDDEN).json({message: NOT_LOGIN_MESSAGE});
+  } else {
+    boards.createNewColumn(req.body, username, boardId, function(result) {
+      if (result === 'error' || !result) {
+        res.status(INTERNAL_SERVER_ERROR).json({message: SERVER_ERROR_MESSAGE});
+      } else if (result === 'notFound') {
+        res.json({message: NO_BOARD_MESSAGE});
+      } else {
+        res.status(STATUS_OK).json({message: 'Add new column success!'});
+      }
+    });
+  }
 });
 
 router.delete('/boards/:id', function(req, res) {
@@ -134,12 +157,12 @@ router.delete('/boards/:id', function(req, res) {
   let boardId = req.params.id;
 
   if (!username) {
-    res.status(STATUS_FORBIDDEN).json({message: LOGIN_MESSAGE});
+    res.status(STATUS_FORBIDDEN).json({message: NOT_LOGIN_MESSAGE});
   } else {
     boards.deleteboardId(username, boardId, function(result) {
       if (result === 'error' || !result) {
         res.status(INTERNAL_SERVER_ERROR).json({message: SERVER_ERROR_MESSAGE});
-      } else if (result === 'notFind' || result.result.n === NO_INFO) {
+      } else if (result === 'notFound' || result.result.n === NO_INFO) {
         res.json({message: NO_BOARD_MESSAGE});
       } else {
         res.status(STATUS_OK).json({message: DELETE_BOARD_MESSAGE});
