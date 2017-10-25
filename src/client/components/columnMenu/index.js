50848abaff84a52dea6b5d2c203328e13b495019
AlexanderZhao <zhaomengzhe@qq.com>
2017-10-25 18:56:35 +0800

    add board menu

---
+++ src/client/components/columnMenu/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import './index.scss';
+
+class ColumnMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown',
+      this.handleClickOutside.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown',
+      this.handleClickOutside.bind(this));
+  }
+
+  handleClickOutside(ev) {
+    if (this.menu && !this.menu.contains(ev.target)) {
+      this.props.closeDropDownMenu();
+    }
+  }
+
+  render() {
+    if (this.props.isEditing) {
+      return (
+        <div className="column-menu"
+          ref={(elem) => {
+            this.menu = elem;
+          }}>
+          <p>Column Actions</p>
+          <ul>
+            <li>Rename Column</li>
+            <li>Add Card</li>
+            <li>Delete Column</li>
+            <li>Move Column To</li>
+          </ul>
+        </div>
+      );
+    }
+    return null;
+  }
+}
+
+export default ColumnMenu;
