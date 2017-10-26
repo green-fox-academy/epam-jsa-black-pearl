85f5155a01b39ca771532dc3fe5d6c021175186b
ChrisH404 <huangchengcq@gmail.com>
2017-10-26 15:39:33 +0800

    cardname -> cardName

--- src/client/components/boardCard/index.js
+++ src/client/components/boardCard/index.js
@@ -10,7 +10,7 @@ class Cards extends React.Component {
   render() {
     return (
       <div className="board-card">
-        <p className="card-title">{this.props.card.cardname}</p>
+        <p className="card-title">{this.props.card.cardName}</p>
         <p className="card-subtitle">{this.props.card.cardsubname}</p>
         <div className="card-info">
           <div className="card-footer">
