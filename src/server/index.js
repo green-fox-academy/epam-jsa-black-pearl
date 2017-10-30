62d7317343319abf622043b820afe40efd8bcf35
AlfredWei0420 <31198072+AlfredWei0420@users.noreply.github.com>
2017-10-30 17:31:40 +0800

    feat(agt-67): PUT /boards/:id/columns/:columsid (#79)
    * import UglifyJSPlugin
    
    * merge from develop and fix a small lint problem for boards.js
    
    * remove the include
    
    * iphone color fixed
    
    * fix with scss error
    
    * remove warnings from uglify
    
    * add column function
    
    * fix register page consistency
    
    * init function
    
    * first function
    
    * test functionality
    
    * merge from develop
    
    * solve confilict
    
    * function works with err handling
    
    * remove unnecsssary part

--- src/server/index.js
+++ src/server/index.js
@@ -33,7 +33,7 @@ const NO_COLUMN_MESSAGE = 'No such column found!';
 const DELETE_COLUMN_MESSAGE = 'Delete column success!';
 const NO_CARD_MESSAGE = 'No such card found!';
 const DELETE_CARD_MESSAGE = 'Delete card success!';
-
+const MODIFY_COLUMN_MESSAGE = 'Modify column success!';
 
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 app.use(bodyParser.json());
@@ -237,6 +237,30 @@ router.delete('/boards/:id/columns/:columsid/cards/:cardsid', function(req, res)
         res.status(NOT_FOUND).json({message: NO_CARD_MESSAGE});
       } else {
         res.status(STATUS_OK).json({message: DELETE_CARD_MESSAGE});
+      }
+    });
+  }
+});
+
+router.put('/boards/:id/columns/:columsid', function(req, res) {
+  let username = jwtVerify(req.headers.token);
+  let boardId = req.params.id;
+  let columnsId = req.params.columsid;
+
+  if (!username) {
+    res.status(STATUS_FORBIDDEN).json({message: NOT_LOGIN_MESSAGE});
+  } else if (!req.is('application/json')) {
+    res.status(BAD_REQUEST).json({message: CONTENT_TYPE_ERROR_MESSAGE});
+  } else if (!req.body.columnName || req.body.columnName === ' ') {
+    res.status(BAD_REQUEST).json({message: MISSING_FIELD_MESSAGE});
+  } else {
+    boards.modifyColumnName(req.body, username, boardId, columnsId, function(result) {
+      if (result === 'error' || !result) {
+        res.status(INTERNAL_SERVER_ERROR).json({message: SERVER_ERROR_MESSAGE});
+      } else if (result === 'notFound') {
+        res.status(NOT_FOUND).json({message: NO_COLUMN_MESSAGE});
+      } else {
+        res.status(STATUS_OK).json({message: MODIFY_COLUMN_MESSAGE});
       }
     });
   }
