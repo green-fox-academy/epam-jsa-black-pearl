b49c5e279dc5f270ff3b76a07fe58da868d71c2f
AlfredWei0420 <31198072+AlfredWei0420@users.noreply.github.com>
2017-10-30 14:36:03 +0800

    fix(app): lint (#78)
    * import UglifyJSPlugin
    
    * merge from develop and fix a small lint problem for boards.js
    
    * remove the include
    
    * iphone color fixed
    
    * fix with scss error
    
    * remove warnings from uglify
    
    * fix register page consistency
    
    * remove webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: [
